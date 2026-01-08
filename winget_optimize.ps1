981c29fd97eb3f4f943b76b625d76eec05c4549b
ИEØ_ΙΙØZ <22034115+Neo1102@users.noreply.github.com>
2026-01-08 15:58:57 +0800

    Update winget_optimize.ps1

--- winget_optimize.ps1
+++ winget_optimize.ps1
@@ -1,4 +1,9 @@
 $jsonpath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+if (-not (Test-Path $jsonpath)) {
+    $defaultSettings = iwr -useb "https://raw.githubusercontent.com/Neo1102/Twitch-Channel-Points-Miner-Auto-Deploy/main/settings.json"
+    $defaultSettings.Content | Set-Content $jsonpath -Encoding UTF8
+	exit 0
+}
 $json = (Get-Content $jsonpath -Raw -Encoding UTF8) -replace '^\s*//.*?(?=\r?\n|$)', '' -replace '\n\s*//.*?(?=\r?\n|$)', ''| ConvertFrom-Json
 if ($json.network -and $json.network.downloader -eq 'wininet') {exit 0}
 $json| Add-Member -MemberType NoteProperty -Name 'network' -Value (@{downloader='wininet'}) -Force
