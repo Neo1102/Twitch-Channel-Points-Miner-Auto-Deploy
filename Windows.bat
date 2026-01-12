c79ea3079b384c1a8a2d8b5378e66772e0782e40
ИEØ_ΙΙØZ <22034115+Neo1102@users.noreply.github.com>
2026-01-12 16:46:00 +0800

    Update Windows.bat

--- Windows.bat
+++ Windows.bat
@@ -9,16 +9,9 @@ if "%errorlevel%"=="0" set Terminal=wt.exe
 if exist "C:\Program Files (x86)\gsudo\Current\gsudo.exe" sudo "%~s0" & exit /B
 if exist "C:\Program Files\gsudo\Current\gsudo.exe" sudo "%~s0" & exit /B
 :: If Windows Terminal is installed, use it.
-if exist "%SYSTEMROOT%\System32\Cscript.exe" (
-    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "%Terminal%", "cmd /c %~s0", "", "runas", 1 > "%temp%\getadmin.vbs"
-    cscript //nologo "%temp%\getadmin.vbs"
-    exit /b
-) else (
-    powershell -Command "Start-Process -Verb RunAs -FilePath '%Terminal%' -ArgumentList 'cmd /c %~s0'"
-    exit /b
-)
+Powershell -Command "Start-Process %Terminal% -ArgumentList 'cmd','/c','\"%~s0\"' -Verb RunAs"
+exit /b
 :gotAdmin
-if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
 pushd "%CD%" && CD /D "%~dp0"
 cls
 :: BatchGotAdmin (Run as Admin code ends)
@@ -30,23 +23,51 @@ setlocal enabledelayedexpansion
 title Twitch Channel Points Miner v2
 call :ConnectionCheck https://www.twitch.tv/
 set Startup="%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
-Powershell "if ((Get-ExecutionPolicy -List | Where-Object {$_.Scope -eq \"LocalMachine\"}).ExecutionPolicy -ne \"RemoteSigned\") { Set-ExecutionPolicy -ExecutionPolicy RemoteSigned }"
-if not exist Auto.bat echo set Auto=False>Auto.bat
 if exist ScriptUpdate.bat del ScriptUpdate.bat
+if not exist Auto.bat echo set Auto=False>Auto.bat
+Powershell wget -Uri "https://raw.githubusercontent.com/chocolatey/choco/master/src/chocolatey.resources/redirects/RefreshEnv.cmd" -OutFile "RefreshEnv.cmd"
+Powershell "if ((Get-ExecutionPolicy -List | Where-Object {$_.Scope -eq \"LocalMachine\"}).ExecutionPolicy -ne \"RemoteSigned\") { Set-ExecutionPolicy -ExecutionPolicy RemoteSigned }"
+Powershell "if ((Get-WindowsCapability -Online -Name "*WMIC*" | Where-Object State -eq 'NotPresent')) { Add-WindowsCapability -Online -Name "WMIC~~~~" }"
+Powershell "Get-AppxPackage|Select Name"|findstr /i AppInstaller >nul
+if "%errorlevel%"=="0" (
+		Powershell -command "irm https://raw.githubusercontent.com/Neo1102/Twitch-Channel-Points-Miner-Auto-Deploy/refs/heads/main/winget_optimize.ps1 | iex"
+		winget upgrade Microsoft.AppInstaller --source winget --silent --accept-source-agreements --accept-package-agreements
+	) else (
+		start "" /wait Powershell -command "irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex"
+		Powershell -command "irm https://raw.githubusercontent.com/Neo1102/Twitch-Channel-Points-Miner-Auto-Deploy/refs/heads/main/winget_optimize.ps1 | iex"
+	)
+winget list --accept-source-agreements > List.txt
+findstr /i "Python.PythonInstallManager " List.txt >nul
+if "%errorlevel%"=="0" ( set Action=upgrade ) else ( set Action=install )
+winget %Action% Python.PythonInstallManager --silent --accept-source-agreements --accept-package-agreements
+
+findstr /i "Git.Git " List.txt >nul
+if "%errorlevel%"=="0" ( set Action=upgrade ) else ( set Action=install )
+winget %Action% Git.Git --silent --accept-source-agreements --accept-package-agreements
+   
+findstr /i "gsudo" List.txt >nul
+if "%errorlevel%"=="0" ( set Action=upgrade ) else ( set Action=install )
+winget %Action% gerardog.gsudo --silent --accept-source-agreements --accept-package-agreements
+sudo config --enable normal
+
+findstr /i "Microsoft.WindowsTerminal" List.txt >nul
+if "%errorlevel%"=="0" ( set Action=upgrade ) else ( set Action=install )
+winget %Action% Microsoft.WindowsTerminal --silent --accept-source-agreements --accept-package-agreements
+REG ADD "HKCU\Console\%%Startup" /V DelegationConsole /T REG_SZ /D "{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}" /F
+REG ADD "HKCU\Console\%%Startup" /V DelegationTerminal /T REG_SZ /D "{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}" /F
+Powershell -command "irm https://raw.githubusercontent.com/Neo1102/Twitch-Channel-Points-Miner-Auto-Deploy/refs/heads/main/wt_modify.ps1 | iex"
+
+findstr /i /C:"Notepad++ (64-bit x64)" List.txt >nul
+if "%errorlevel%"=="0" ( set Action=upgrade ) else ( set Action=install )
+winget %Action% Notepad++.Notepad++ --silent --accept-source-agreements --accept-package-agreements
+
+del /q List.txt
 set Status=Check
+call RefreshEnv.cmd
+cls
 call :Check_Script_Update
 call :Python
 call :Miner
-Powershell "Get-AppxPackage|Select Name"|findstr /i AppInstaller >nul
-if not "%errorlevel%"=="0" start "" /wait Powershell -command "irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex"
-winget list --accept-source-agreements > List.txt
-findstr /i "gsudo" List.txt >nul
-if not "%errorlevel%"=="0" winget install gerardog.gsudo --accept-source-agreements >nul
-findstr /i "Microsoft.WindowsTerminal" List.txt >nul
-if not "%errorlevel%"=="0" winget install Microsoft.WindowsTerminal --accept-source-agreements >nul
-findstr /i /C:"Notepad++ (64-bit x64)" List.txt >nul
-if not "%errorlevel%"=="0" winget install Notepad++.Notepad++ --accept-source-agreements >nul
-del /q List.txt
 
 :Menu
 cd /d "%~dp0"
@@ -88,11 +109,10 @@ echo Checking Lasts Python Version ......
 echo.
 set LastsPythonVer=&set PythonURL=&set PyUpdate=
 call :ConnectionCheck https://www.python.org/downloads/
-for /f "delims=" %%i in ('Powershell -EncodedCommand "JABjAG8AbgB0AGUAbgB0ACAAPQAgACgASQBuAHYAbwBrAGUALQBXAGUAYgBSAGUAcQB1AGUAcwB0ACAALQBVAHIAaQAgACIAaAB0AHQAcABzADoALwAvAHcAdwB3AC4AcAB5AHQAaABvAG4ALgBvAHIAZwAvAGQAbwB3AG4AbABvAGEAZABzAC8AIgApAC4AQwBvAG4AdABlAG4AdAANAAoAJAB1AHIAbABQAGEAdAB0AGUAcgBuACAAPQAgACcAaAByAGUAZgA9ACIAKABoAHQAdABwAHMAOgAvAC8AdwB3AHcALgBwAHkAdABoAG8AbgAuAG8AcgBnAFsAXgAiAF0AKwBcAC4AZQB4AGUAKQAiACcADQAKACQAcgBlAGcAZQB4ACAAPQAgAFsAcgBlAGcAZQB4AF0AOgA6AG4AZQB3ACgAJAB1AHIAbABQAGEAdAB0AGUAcgBuACkADQAKACQAbQBhAHQAYwBoACAAPQAgACQAcgBlAGcAZQB4AC4ATQBhAHQAYwBoACgAJABjAG8AbgB0AGUAbgB0ACkADQAKAGkAZgAgACgAJABtAGEAdABjAGgALgBTAHUAYwBjAGUAcwBzACkAIAB7AA0ACgAgACAAIAAgACQAZABvAHcAbgBsAG8AYQBkAFUAcgBsACAAPQAgACQAbQBhAHQAYwBoAC4ARwByAG8AdQBwAHMAWwAxAF0ALgBWAGEAbAB1AGUADQAKAAkAVwByAGkAdABlAC0ASABvAHMAdAAgACQAZABvAHcAbgBsAG8AYQBkAFUAcgBsAA0ACgB9AA=="') do set PythonURL=%%i
-for /f "delims=/ tokens=5" %%i in ('echo %PythonURL%') do set LastsPythonVer=%%i
-python --version >nul
-if not "%errorlevel%"=="0" goto DownloadPython
-for /f "tokens=2" %%i in ('python --version^|findstr /i "Python"') do set PythonVer=%%i
+for /f %%i in ('Powershell "(py list --online -1 -f=json | ConvertFrom-Json).versions[0].'sort-version'"') do set LastsPythonVer=%%i
+py list|findstr /i /C:"No runtimes. " >nul
+if "%errorlevel%"=="0" goto DownloadPython
+for /f %%i in ('Powershell "(py list -f=json | ConvertFrom-Json).versions[0].'sort-version'"') do set PythonVer=%%i
 if "%PythonVer%"=="%LastsPythonVer%" (
   if "%Status%"=="Check" goto :eof
   echo No Update Available
@@ -106,44 +126,43 @@ echo Current Version : %PythonVer%
 echo Lasts Version : %LastsPythonVer%
 choice /M:"Do you want to update Python?"
 if "%errorlevel%"=="2" goto :eof
+echo  Do you want to uninstall the old version of Python?
+echo  (1) No, keep old version.
+echo  (2) Yes, uninstall but keeps config files. Reinstall reuses settings.
+echo  (3) Yes, uninstall python and config files for full cleanup.
+choice /c 123
+if "%errorlevel%"=="3" py uninstall -y -purge
+if "%errorlevel%"=="2" py uninstall %PythonVer% -y
 :DownloadPython
-echo Downloading Python %LastsPythonVer% Installer ......
-echo.
-cd /d "%Temp%"
-Powershell wget -Uri "%PythonURL%" -OutFile "python.exe"
-echo Installing Python %LastsPythonVer% ......
-if exist python.exe python.exe /passive InstallAllUsers=1 AppendPath=1 PrependPath=1
-echo Cleaning File ......
-echo.
-del /q python.exe
-cd /d "%~dp0"
-Powershell wget -Uri "https://raw.githubusercontent.com/chocolatey/choco/master/src/chocolatey.resources/redirects/RefreshEnv.cmd" -OutFile "RefreshEnv.cmd"
+py install %LastsPythonVer% -y
+py install --configure
 call RefreshEnv.cmd
-for /f "tokens=2" %%i in ('python --version^|findstr /i "Python"') do set PythonVer=%%i
+for /f %%i in ('Powershell "(py list -f=json | ConvertFrom-Json).versions[0].'sort-version'"') do set PythonVer=%%i
 call :Requirements
 set PyUpdate=
-if not "%Status%"=="Check" pause
+if not "%Status%"=="Check" timeout 3
 goto :eof
 
 
 :Miner
 echo Checking Lasts Miner Version ......
 echo.
-set MinerVer=none&set GitHubVer=&set MinerUpdate=
-if not exist .\TwitchChannelPointsMiner\__init__.py goto DownloadMiner
-for /f tokens^=2^ delims^=^" %%i in ('findstr /i "version" .\TwitchChannelPointsMiner\__init__.py') do set MinerVer=%%i
 call :ConnectionCheck https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2/
-for /f tokens^=2^ delims^=^" %%i in ('Powershell wget -Uri "https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2/raw/master/TwitchChannelPointsMiner/__init__.py"^|findstr /i __version__') do set GitHubVer=%%i
-if "%MinerVer%"=="%GitHubVer%" (
+set MinerVer=none&set LastsMinerVer=&set MinerUpdate=
+if not exist .\TwitchChannelPointsMiner\__init__.py goto DownloadMiner
+for /f "tokens=2" %%i in ('git ls-remote --tags https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2.git') do set LastsMinerVer=%%i
+set LastsMinerVer=%LastsMinerVer:refs/tags/=%
+for /f tokens^=2^ delims^=^" %%i in ('findstr /i "version" .\TwitchChannelPointsMiner\__init__.py') do set MinerVer=%%i
+if "%MinerVer%"=="%LastsMinerVer%" (
   if "%Status%"=="Check" goto :eof
   echo No Update Available
-  set Msg=Do you want to download the main program again?
+  set Msg=Do you still want to re-download the Miner Program no matter what?
   )
-if not "%MinerVer%"=="%GitHubVer%" (
+if not "%MinerVer%"=="%LastsMinerVer%" (
   if "%Status%"=="Check" set MinerUpdate=[Update Available]&goto :eof
   echo Update Available
   echo Current  Version : %MinerVer%
-  echo Lasts Version : %GitHubVer%
+  echo Lasts Version : %LastsMinerVer%
   set Msg=Do you want to update Miner program?
   )
 echo.
@@ -151,20 +170,11 @@ choice /M:"%Msg%"
 if "%errorlevel%"=="2" goto :eof
 :DownloadMiner
 echo Downloading Lasts Miner Program ......
-cd /d "%Temp%"
-Powershell wget -Uri "https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2/archive/refs/heads/master.zip" -OutFile "master.zip"
-echo Decompression Master Program ......
-echo.
-tar -xf master.zip
+git clone https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2.git
 echo Moving Master Program ......
 echo.
-xcopy /e /y .\Twitch-Channel-Points-Miner-v2-master "%~dp0"
-echo Cleaning File ......
-echo.
-del /q /s master.zip Twitch-Channel-Points-Miner-v2-master
-rmdir /q /s Twitch-Channel-Points-Miner-v2-master
-cd /d "%~dp0"
-if exist getPython.ps1 del getPython.ps1
+robocopy "%~dp0Twitch-Channel-Points-Miner-v2" "." /E /MOV /W:1
+rmdir /q /s Twitch-Channel-Points-Miner-v2
 call :Requirements
 echo.
 echo                         --- Complete ---
@@ -182,12 +192,12 @@ if not exist .\requirements.txt goto :eof
 echo Installing Requirements ......
 echo.
 Powershell wget -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
-python get-pip.py
-python -m pip install --upgrade pip
-python -m pip install setuptools
-pip install -r requirements.txt
-python setup.py build
-python setup.py install
+py get-pip.py
+py -m pip install --upgrade pip
+py -m pip install setuptools
+py -m pip install -r requirements.txt
+py setup.py build
+py setup.py install
 goto :eof
 
 
@@ -227,10 +237,10 @@ rmdir /s /q TwitchChannelPointsMiner\classes\entities\__pycache__
 goto :eof
 
 :ConnectionCheck
-Powershell wget -Uri "%1"^|Select StatusCode|findstr "200" >nul
+Powershell -command "(Invoke-WebRequest -Uri '%1' -TimeoutSec 5 -UseBasicParsing -UserAgent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36').StatusCode"|findstr "200" >nul
 if "%errorlevel%"=="0" goto :eof
 if "%Status%"=="Startup" goto ConnectionCheck
-if "%Status%"=="Check" goto menu
+if "%Status%"=="Check" goto :eof
 echo Currently unable to connect to %1.
 echo Please check your internet connection or try again later.
 timeout 5
@@ -255,4 +265,3 @@ echo start "" /D "%~dp0" %~nx0>>ScriptUpdate.bat
 echo exit>>ScriptUpdate.bat
 start ScriptUpdate.bat
 exit
-
