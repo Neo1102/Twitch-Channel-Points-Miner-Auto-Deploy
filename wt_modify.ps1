996b6a282d6e7923ec74ded371bd3fb2f80eacd6
ИEØ_ΙΙØZ <22034115+Neo1102@users.noreply.github.com>
2026-01-12 10:39:20 +0800

    Update wt_modify.ps1

--- wt_modify.ps1
+++ wt_modify.ps1
@@ -2,4 +2,5 @@ $jsonpath = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\
 $json = Get-Content $jsonpath -Raw | ConvertFrom-Json
 $json.defaultProfile    = '{0caa0dad-35be-5f56-a8ff-afceeeaa6101}'
 $json.windowingBehavior = 'useAnyExisting'
+$json| Add-Member -MemberType NoteProperty -Name 'windowingBehavior' -Value 'useAnyExisting' -Force
 $json | ConvertTo-Json -Depth 32 | Set-Content -Path $jsonpath -Encoding UTF8
